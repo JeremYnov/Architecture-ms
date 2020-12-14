1ee20ad07315c0230ca1c0637365c8cee48facce
JeremYnov <jeremy.richard@ynov.com>
2020-12-14 15:09:38 +0100

    added correction and controller connection

--- category-api/src/main/java/ms/ynov/categoryapi/controller/CategoryController.java
+++ category-api/src/main/java/ms/ynov/categoryapi/controller/CategoryController.java
@@ -1,8 +1,10 @@
 package ms.ynov.categoryapi.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import ms.ynov.categoryapi.model.Category;
 import ms.ynov.categoryapi.repository.CategoryRepository;
 
 @RestController
@@ -10,6 +12,12 @@ public class CategoryController {
 
 	@Autowired
 	private CategoryRepository categoryRepository;
+	
+	@GetMapping("/category")
+	public Iterable<Category> getCategories(){
+		Iterable<Category> categories = categoryRepository.findAll();
+		return categories;
+	}
 	
 	
 }
