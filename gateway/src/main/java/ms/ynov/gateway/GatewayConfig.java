e4669242ae29e694782a5770a6936b342353d897
DavidMTdev <david.teixeira31@gmail.com>
2020-12-14 12:25:09 +0100

    Add eureka server & gateway

---
+++ gateway/src/main/java/ms/ynov/gateway/GatewayConfig.java
@@ -0,0 +1,17 @@
+package ms.ynov.gateway;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+    
+    @Bean
+	public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+		return builder.routes()
+		.route(r -> r.path("/user/**").uri("lb://userapi"))
+		.build();
+	}
+}
