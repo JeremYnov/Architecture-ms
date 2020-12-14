86ab865a278a9370bc0f84b4d91f9e411927726a
JeremYnov <jeremy.richard@ynov.com>
2020-12-14 15:26:21 +0100

    correction of repository file

--- category-api/src/main/java/ms/ynov/categoryapi/repository/CategoryRepository.java
+++ category-api/src/main/java/ms/ynov/categoryapi/repository/CategoryRepository.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Repository;
 
 import ms.ynov.categoryapi.model.Category;
 
+@Repository
 public interface CategoryRepository extends CrudRepository<Category, Integer> {
 
 }
